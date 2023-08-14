25da23c008e630111c25a982cc30c1b30e1c276d
Oksana <Oksana240884>
2023-08-14 22:56:20 +0300

    Initial commit

---
+++ assets/css/index.js
@@ -0,0 +1,30 @@
+const multiplier1 = 2;
+const multiplier2 = 3;
+console.log(multiplier1 * multiplier2);
+
+const divisor1 = 6;
+const divisor2 = 2;
+console.log(divisor1 / divisor2);
+
+const application1 = 5;
+const application2 = 6;
+console.log(application1 + application2);
+
+let number = 11;
+console.log(number);
+
+let wort = ('true');
+console.log(wort);
+
+let js = String(100);
+console.log(js);
+
+
+let num = 1;
+num += 11;
+num -= -11;
+num *= 11;
+num /= 11;
+num += 1;
+num -= -1;
+console.log(num);
---
+++ assets/css/loops.js
@@ -0,0 +1,18 @@
+/*let imputNumber = Number(prompt('Введіть число'));
+let power = imputNumber**2;
+console.log(power);*/
+
+/*let imputNumber1 = Number(prompt('Введіть перше число'));
+let imputNumber2 = Number(prompt('Введіть друге число'));
+let num1 = (imputNumber1 + imputNumber2)  /  2;
+console.log(num1);*/
+
+/*let imputMinute = Number(prompt('Введіть число хвилин'));
+let sec = imputMinute * 60;
+console.log(sec);*/
+
+
+let greeting = 'Hello, ';
+let userName = prompt(`введіть ім'я`);
+let duo = greeting + userName;
+console.log(duo);
